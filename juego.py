833da077f6315bd4e23dc320669bdfb9c496f093
yelmis <pichardoyelmis7@gmail.com>
2024-11-23 14:31:09 -0400

    mejora de codigo en todo  el programa

--- juego.py
+++ juego.py
@@ -2,107 +2,157 @@ import turtle
 import time
 import random
 
-delay = 0.1
-body_segments = {}
+posponer = 0.1
 
-# Configuración de la ventana
+#Marcador
+score = 0
+high_score = 0
+
+#Configuracion de la ventana
 wn = turtle.Screen()
-wn.title("Juego Snake")
-wn.setup(width=600, height=600)
-wn.bgcolor("blue")
+wn.title("Snake Game 1.5.2")
+wn.bgcolor("black")
+wn.setup(width = 600, height = 600)
+wn.tracer(0)
 
-# Configuración de la cabeza de la serpiente
-head = turtle.Turtle()
-head.speed(0)
-head.shape('square')
-head.color('red')
-head.penup()
-head.goto(0, 0)
-head.direction = "stop"
+#Cabeza serpiente
+cabeza = turtle.Turtle()
+cabeza.speed(0)
+cabeza.shape("square")
+cabeza.color("white")
+cabeza.penup()
+cabeza.goto(0,0)
+cabeza.direction = "stop"
 
-# Configuración de la comida
-food = turtle.Turtle()
-food.speed(0)
-food.shape("dog")
-food.color("green")
-food.penup()
-food.goto(0, 100)
+#Comida
+comida = turtle.Turtle()
+comida.speed(0)
+comida.shape("square")
+comida.color("red")
+comida.penup()
+comida.goto(0,100)
 
-# Movimiento de la serpiente
+#Segmentos
+segmentos = []
+
+#Texto
+texto = turtle.Turtle()
+texto.speed(0)
+texto.color("white")
+texto.penup()
+texto.hideturtle()
+texto.goto(0,260)
+texto.write("Score: 0     High Score: 0", align = "center", font =("Courier", 24, "normal"))
+
+#Funciones
+def arriba():
+    cabeza.direction = "up"
+def abajo():
+    cabeza.direction = "down"
+def izquierda():
+    cabeza.direction = "left"
+def derecha():
+    cabeza.direction = "right"
+
 def mov():
-    if head.direction == "up":
-        y = head.ycor()
-        head.sety(y + 20)
-    if head.direction == "down":
-        y = head.ycor()
-        head.sety(y - 20)
-    if head.direction == "right":
-        x = head.xcor()
-        head.setx(x + 20)
-    if head.direction == "left":
-        x = head.xcor()
-        head.setx(x - 20)
+    if cabeza.direction == "up":
+        y = cabeza.ycor()
+        cabeza.sety(y + 20)
+        
+    if cabeza.direction == "down":
+        y = cabeza.ycor()
+        cabeza.sety(y - 20)
 
-# Funciones para cambiar la dirección
-def dirUp():
-    if head.direction != "down":
-        head.direction = "up"
+    if cabeza.direction == "left":
+        x = cabeza.xcor()
+        cabeza.setx(x - 20)
 
-def dirDown():
-    if head.direction != "up":
-        head.direction = "down"
+    if cabeza.direction == "right":
+        x = cabeza.xcor()
+        cabeza.setx(x + 20)
 
-def dirRight():
-    if head.direction != "left":
-        head.direction = "right"
-
-def dirLeft():
-    if head.direction != "right":
-        head.direction = "left"
-
-# Conexión de teclas
+#Teclado
 wn.listen()
-wn.onkeypress(dirUp, "Up")
-wn.onkeypress(dirDown, "Down")
-wn.onkeypress(dirRight, "Right")
-wn.onkeypress(dirLeft, "Left")
-
-# Bucle principal del juego
+wn.onkeypress(arriba, "Up")
+wn.onkeypress(abajo, "Down")
+wn.onkeypress(izquierda, "Left")
+wn.onkeypress(derecha, "Right")
+    
 while True:
     wn.update()
 
-    # Comprobar si la cabeza de la serpiente toca la comida
-    if head.distance(food) < 20:
-        x = random.randint(-290, 290)
-        y = random.randint(-290, 290)
-        food.goto(x, y)
-         #cofiguracion de los segmet
-         new_segments = turtle.Turtle() 
-         new_segments.speed(0)
-         new_segments.shape('square')
-         new_segments.color('yellow')
-         new_segments.penup()
-         body_segments.append(new_segments)
-         print(body_segments)
+    #Colisiones bordes
+    if cabeza.xcor() > 280 or cabeza.xcor() < -280 or cabeza.ycor() > 280 or cabeza.ycor() < -280:
+        time.sleep(1)
+        cabeza.goto(0,0)
+        cabeza.direction = "stop"
+
+        #Esconder los segmentos
+        for segmento in segmentos:
+            segmento.goto(1000,1000)
+
+        #Limpiar lista de segmentos
+            segmento.clear()
+
+        #Resetear marcador
+        score = 0
+        texto.clear()   
+        texto.write("Score: {}     High Score: {}".format(score, high_score), 
+                align = "center", font =("Courier", 24, "normal"))
+
+    #Colisiones comida
+    if cabeza.distance(comida) < 20: 
+        x = random.randint(-280,280)
+        y = random.randint(-280,280)
+        comida.goto(x,y)
+
+        nuevo_segmento = turtle.Turtle()
+        nuevo_segmento.speed(0)
+        nuevo_segmento.shape("square")
+        nuevo_segmento.color("red")
+        nuevo_segmento.penup()
+        segmentos.append(nuevo_segmento)
+
+        #Aumentar marcador 
+        score += 1
          
-         totlseg = len(body_segments)
-         print(totlseg)
- 
-        for i in range(totlseg - 1, 0, -1):
-            x = body_segments[i - 1].xcor()
-            y = body_segments[i - 1].ycor()
-            body_segments [i]. goto(x, y)
-            
-          # Mover la cabeza de la serpiente
-if totlseg > 0:
-    x = head.xcor()
-    y = head.ycor()
-    body_segments[0].goto(x, y)
-            
+        if score > high_score: 
+           high_score = score 
 
+        texto.clear()   
+        texto.write("Score: {}    High Score: {}".format(score, high_score), 
+                align = "center", font =("Courier", 24, "normal"))
+    
+    #Mover el cuerpo de la serpiente
+    totalSeg = len(segmentos)
+    for index in range(totalSeg -1, 0, -1):
+        x = segmentos[index - 1].xcor()
+        y = segmentos[index - 1].ycor()
+        segmentos[index].goto(x,y)
 
+    if totalSeg > 0:
+        x = cabeza.xcor()
+        y = cabeza.ycor()
+        segmentos[0].goto(x,y)
 
     mov()
-    time.sleep(0.1)
 
-turtle.done()
+    #Colisiones con el cuerpo
+    for segmento in segmentos:
+        if segmento.distance(cabeza) < 20:
+            time.sleep
+            cabeza.goto(0,0)
+            cabeza.direction = "stop"
+
+            #Esconder los segmentos
+            for segmento in segmentos:
+                segmento.goto(1000,1000)
+
+            segmentos.clear()
+
+            score = 0
+            texto.clear()   
+            texto.write("Score: {}    High Score: {}".format(score, high_score), 
+                align = "center", font =("Courier", 24, "normal"))
+        
+    time.sleep(posponer)
